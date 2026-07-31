fdf5141c4c318444b59f7e3cb5b4c0e335852b85
Ilya Gulya <ilya@gulya.me>
2026-07-31 16:10:00 +0000

    Preserve retained vchroot capability across bootstrap exec

--- src/vchroot/vchroot.c
+++ src/vchroot/vchroot.c
@@ -3,6 +3,8 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <limits.h>
+#include <sys/stat.h>
 
 extern int lkm_call(int nr, ...);
 extern int __darling_vchroot(int dfd);
@@ -11,24 +13,32 @@ int main(int argc, const char** argv)
 {
     if (argc < 3)
 	{
-		fprintf(stderr, "vchroot <dir> <binary> [args...]\n");
+		fprintf(stderr, "vchroot <directory-fd> <binary> [args...]\n");
 		return 1;
 	}
 
-	char buf[4096];
-	sprintf(buf, "%s%s", argv[1], argv[2]);
-
-	if (access(buf, F_OK) != 0)
+	char* end = NULL;
+	errno = 0;
+	long parsed_fd = strtol(argv[1], &end, 10);
+	struct stat prefix_stat;
+	if (errno != 0 || end == argv[1] || *end != '\0' ||
+		parsed_fd < 0 || parsed_fd > INT_MAX ||
+		fstat((int)parsed_fd, &prefix_stat) != 0 ||
+		!S_ISDIR(prefix_stat.st_mode))
 	{
-		fprintf(stderr, "Target executable not found: %s\n", buf);
+		fprintf(stderr, "Invalid retained vchroot directory capability\n");
+		return 1;
+	}
+	int dfd = (int)parsed_fd;
+
+	const char* target = argv[2];
+	while (*target == '/')
+		target++;
+	if (*target == '\0' || faccessat(dfd, target, F_OK, 0) != 0)
+	{
+		fprintf(stderr, "Target executable not found below retained prefix: %s\n",
+			argv[2]);
 		return 5;
-	}
-
-	int dfd = open(argv[1], O_RDONLY | O_DIRECTORY);
-	if (dfd == -1)
-	{
-		perror("open");
-		return 1;
 	}
 
 	if (fchdir(dfd) == -1)
@@ -54,4 +64,3 @@ int main(int argc, const char** argv)
 
 	return 4;
 }
-
